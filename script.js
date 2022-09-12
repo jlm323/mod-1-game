d932a20e750ef4f3ee6814b26a24ce6028202cdc
Jessica Milewski <jlm323@pitt.edu>
2022-09-12 16:29:19 -0400

    fonts, images, css

---
+++ script.js
@@ -0,0 +1,22 @@
+// player vs computer
+// player chooses between rock, paper, or scissors
+
+// rock -   images/skel-rock.png
+
+// paper -  images/skel-paper.png
+
+// scissors - images/skel-scissors.png
+
+// 3 rounds
+// game round begins on a countdown from 3 
+// player has to win 2 out of 3 rounds to win the whole game
+// randomize computer choice for each round
+// player chooses between r, p, s for each round
+// write func that says 
+    // rock beats scissors
+    // scissors beats paper
+    // paper beats rock
+// keep track of how many rounds player and computer has won
+// button to reset during the game, makes game start over
+// play again button when game is won or lost
+// if a draw, the round starts over
