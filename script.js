d9ba63ee5eb08d6ce6fe503b09e6740708e0c530
Jessica Milewski <jlm323@pitt.edu>
2022-09-13 16:11:43 -0400

    game functions, positioning

--- script.js
+++ script.js
@@ -20,3 +20,72 @@
 // button to reset during the game, makes game start over
 // play again button when game is won or lost
 // if a draw, the round starts over
+
+const playerChoice = document.getElementById('player-choice');
+const computerChoice = document.getElementById('computer-choice');
+const options = document.querySelectorAll('.choice');
+const resultArea = document.getElementById('result');
+const resetBtn = document.querySelector('reset');
+let rockPic = document.getElementById('rockPic');
+let paperPic = document.getElementById('paperPic');
+let scissorsPic = document.getElementById('scissorsPic');
+let playerPick
+let computerPick
+let result
+
+options.forEach(options => options.addEventListener('click', (e) => {
+    playerPick = e.target.id
+    playerChoice.innerHTML = playerPick
+    randomComputer()
+    game()
+}))
+
+function randomComputer() {
+    const randomChoice = Math.floor(Math.random() * options.length) + 1;
+    console.log(randomChoice)
+    if (randomChoice === 1) {
+        computerPick = 'rock'
+        
+    }
+    if (randomChoice === 2) {
+        computerPick = 'paper'
+        
+    }
+    if (randomChoice === 3) {
+        computerPick = 'scissors'
+        
+    }
+    computerChoice.innerHTML = computerPick
+}
+
+function game(){
+    if (playerPick === 'scissors' && computerPick === 'paper') {
+        result = 'You win!'
+    }
+
+    if (playerPick === 'rock' && computerPick === 'scissors') {
+        result = 'You win!'
+    }
+
+    if (playerPick === 'paper' && computerPick === 'rock') {
+        result = 'You win!'
+    }
+
+    if (playerPick === 'scissors' && computerPick === 'rock') {
+        result = 'You lose!'
+    }
+
+    if (playerPick === 'rock' && computerPick === 'paper') {
+        result = 'You lose!'
+    }
+
+    if (playerPick === 'paper' && computerPick === 'scissors') {
+        result = 'You lose!'
+    }
+
+    if (playerPick === computerPick) {
+        result = "It's a draw!"
+    }
+
+    resultArea.innerHTML = result
+}
