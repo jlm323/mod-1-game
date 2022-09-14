eed2290f61c40699145366c92bde127085138477
Jessica Milewski <jlm323@pitt.edu>
2022-09-14 16:20:31 -0400

    corresponding pick with image, styling

--- script.js
+++ script.js
@@ -21,71 +21,147 @@
 // play again button when game is won or lost
 // if a draw, the round starts over
 
-const playerChoice = document.getElementById('player-choice');
-const computerChoice = document.getElementById('computer-choice');
+
+//////////////////////////////////////////////////////////
+
+
+const playerArea = document.getElementById('player-choice');
+const computerArea = document.getElementById('computer-choice');
 const options = document.querySelectorAll('.choice');
 const resultArea = document.getElementById('result');
 const resetBtn = document.querySelector('reset');
-let rockPic = document.getElementById('rockPic');
-let paperPic = document.getElementById('paperPic');
-let scissorsPic = document.getElementById('scissorsPic');
-let playerPick
-let computerPick
-let result
 
+let playerPick;
+let computerPick;
+let result;
+let round = 0;
+const rounds = 3;
+let playerScore = 0;
+let computerScore = 0;
+
+
+// event listener on buttons 
+// depending on choice, related image appears in the player area
 options.forEach(options => options.addEventListener('click', (e) => {
-    playerPick = e.target.id
-    playerChoice.innerHTML = playerPick
+    playerPick = e.target.id;
+    playerArea.innerHTML = playerPick;
+    if (playerPick == 'rock') {
+        let rockPic = document.createElement('img');
+        rockPic.setAttribute('src', '/images/skel-rock.png');
+        rockPic.className = 'choicePic';
+        playerArea.appendChild(rockPic);
+    }
+
+    if (playerPick == 'paper') {
+        let paperPic = document.createElement('img');
+        paperPic.setAttribute('src', '/images/skel-paper.png');
+        paperPic.className = 'choicePic';
+        playerArea.appendChild(paperPic);
+    }
+
+    if (playerPick == 'scissors') {
+        let scissorsPic = document.createElement('img');
+        scissorsPic.setAttribute('src', '/images/skel-scissors.png');
+        scissorsPic.className = 'choicePic';
+        playerArea.appendChild(scissorsPic);
+    }
+    console.log('player choice: ' + playerPick)
     randomComputer()
-    game()
+    gameRound()
+
+    if (round <= rounds) {
+        if (result == 'You win!') {
+            playerScore += 1;
+            resultArea.innerHTML = "Rounds won: " + playerScore;
+        } else if (result == 'You lose!') {
+            computerScore += 1;
+        } 
+        round++; 
+        if (round === rounds) {
+            
+            if (playerScore >= 2) {
+                resultArea.innerHTML = "YOU WON THE GAME!"
+            }
+            if (computerScore >= 2) {
+                resultArea.innerHTML = "YOU LOST THE GAME!"
+            }
+        }
+    }
 }))
 
+// computer randomly picking an option
 function randomComputer() {
     const randomChoice = Math.floor(Math.random() * options.length) + 1;
-    console.log(randomChoice)
+
     if (randomChoice === 1) {
-        computerPick = 'rock'
-        
+        computerPick = 'rock';
     }
+
     if (randomChoice === 2) {
-        computerPick = 'paper'
-        
+        computerPick = 'paper';        
     }
+
     if (randomChoice === 3) {
-        computerPick = 'scissors'
-        
+        computerPick = 'scissors';        
     }
-    computerChoice.innerHTML = computerPick
+    console.log('computer choice: ' + computerPick)
+
+    computerArea.innerHTML = computerPick;
+
+    // once computer picks, related image appears in computer area
+    if (computerPick == 'rock') {
+        let rockPic = document.createElement('img');
+        rockPic.setAttribute('src', '/images/skel-rock.png');
+        rockPic.className = 'choicePic';
+        computerArea.appendChild(rockPic);
+    }
+
+    if (computerPick == 'paper') {
+        let paperPic = document.createElement('img');
+        paperPic.setAttribute('src', '/images/skel-paper.png');
+        paperPic.className = 'choicePic';
+        computerArea.appendChild(paperPic);
+    }
+
+    if (computerPick == 'scissors') {
+        let scissorsPic = document.createElement('img');
+        scissorsPic.setAttribute('src', '/images/skel-scissors.png');
+        scissorsPic.className = 'choicePic';
+        computerArea.appendChild(scissorsPic);
+    }
 }
 
-function game(){
+// results for if player wins, if computer wins, or draw
+function gameRound(){
     if (playerPick === 'scissors' && computerPick === 'paper') {
-        result = 'You win!'
+        result = 'You win!';
     }
 
     if (playerPick === 'rock' && computerPick === 'scissors') {
-        result = 'You win!'
+        result = 'You win!';
     }
 
     if (playerPick === 'paper' && computerPick === 'rock') {
-        result = 'You win!'
+        result = 'You win!';
     }
 
     if (playerPick === 'scissors' && computerPick === 'rock') {
-        result = 'You lose!'
+        result = 'You lose!';
     }
 
     if (playerPick === 'rock' && computerPick === 'paper') {
-        result = 'You lose!'
+        result = 'You lose!';
     }
 
     if (playerPick === 'paper' && computerPick === 'scissors') {
-        result = 'You lose!'
+        result = 'You lose!';
     }
 
     if (playerPick === computerPick) {
-        result = "It's a draw!"
+        result = "It's a draw!";
     }
 
-    resultArea.innerHTML = result
+    resultArea.innerHTML = result;
 }
+
+
