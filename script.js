1eec297d3ad671daea5b43cebc160a21b8e725da
Jessica Milewski <jlm323@pitt.edu>
2022-09-15 12:23:48 -0400

    fixed rounds, added reset, play again btns

--- script.js
+++ script.js
@@ -7,9 +7,9 @@
 
 // scissors - images/skel-scissors.png
 
-// 3 rounds
-// game round begins on a countdown from 3 
-// player has to win 2 out of 3 rounds to win the whole game
+// 5 rounds
+// game round begins on a delay
+// player has to win more rounds than computer within 5 rounds to win the whole game
 // randomize computer choice for each round
 // player chooses between r, p, s for each round
 // write func that says 
@@ -29,22 +29,38 @@ const playerArea = document.getElementById('player-choice');
 const computerArea = document.getElementById('computer-choice');
 const options = document.querySelectorAll('.choice');
 const resultArea = document.getElementById('result');
-const resetBtn = document.querySelector('reset');
+const roundResult = document.querySelector('#roundResult')
+const resetBtn = document.querySelector('.reset');
 
 let playerPick;
 let computerPick;
 let result;
 let round = 0;
-const rounds = 3;
+const rounds = 5;
 let playerScore = 0;
 let computerScore = 0;
 
+// creates score keeping element
+const roundNum = document.createElement('p');
+roundNum.setAttribute('id', 'score');
+
+// creates play again button when game ends
+const playAgain = document.createElement('button');
+playAgain.setAttribute('id', 'play-again');
+playAgain.innerText = "Play Again?";
+playAgain.addEventListener('click', (e) => {
+    location.reload();
+})
 
 // event listener on buttons 
 // depending on choice, related image appears in the player area
 options.forEach(options => options.addEventListener('click', (e) => {
     playerPick = e.target.id;
     playerArea.innerHTML = playerPick;
+    newGame();
+}))
+
+function newGame() {
     if (playerPick == 'rock') {
         let rockPic = document.createElement('img');
         rockPic.setAttribute('src', '/images/skel-rock.png');
@@ -66,28 +82,41 @@ options.forEach(options => options.addEventListener('click', (e) => {
         playerArea.appendChild(scissorsPic);
     }
     console.log('player choice: ' + playerPick)
+    // computer makes random choice:
     randomComputer()
-    gameRound()
+    
+    if (round < rounds) {
+        gameRound();
 
-    if (round <= rounds) {
-        if (result == 'You win!') {
-            playerScore += 1;
-            resultArea.innerHTML = "Rounds won: " + playerScore;
-        } else if (result == 'You lose!') {
-            computerScore += 1;
-        } 
-        round++; 
-        if (round === rounds) {
+        // increases scores of player or computer then increases the round
+        setTimeout(() => {
+            if (result == 'You win!') {
+                playerScore += 1;
+            } else if (result == 'You lose!') {
+                computerScore += 1;
+            } 
+            round++; 
             
-            if (playerScore >= 2) {
-                resultArea.innerHTML = "YOU WON THE GAME!"
+            // updates rounds won aka player score and current round
+            resultArea.appendChild(roundNum);
+            roundNum.innerHTML = `Current round: ${round} <br><br> Rounds won: ${playerScore} / 5`;
+    
+            if (round === rounds) {
+                // player wins game
+                if (playerScore > computerScore) {
+                    roundResult.innerHTML = "YOU WON THE GAME!";
+                    // play again button
+                    roundResult.appendChild(playAgain);
+                } // player loses game
+                if (computerScore > playerScore) {
+                    roundResult.innerHTML = "YOU LOST THE GAME!"
+                    // play again button
+                    roundResult.appendChild(playAgain);
+                }
             }
-            if (computerScore >= 2) {
-                resultArea.innerHTML = "YOU LOST THE GAME!"
-            }
-        }
+        }, 2000);
     }
-}))
+}
 
 // computer randomly picking an option
 function randomComputer() {
@@ -105,30 +134,35 @@ function randomComputer() {
         computerPick = 'scissors';        
     }
     console.log('computer choice: ' + computerPick)
-
-    computerArea.innerHTML = computerPick;
+    setTimeout(() => {
+        computerArea.innerHTML = computerPick;
+    }, 1000);
+    
 
     // once computer picks, related image appears in computer area
-    if (computerPick == 'rock') {
-        let rockPic = document.createElement('img');
-        rockPic.setAttribute('src', '/images/skel-rock.png');
-        rockPic.className = 'choicePic';
-        computerArea.appendChild(rockPic);
-    }
-
-    if (computerPick == 'paper') {
-        let paperPic = document.createElement('img');
-        paperPic.setAttribute('src', '/images/skel-paper.png');
-        paperPic.className = 'choicePic';
-        computerArea.appendChild(paperPic);
-    }
-
-    if (computerPick == 'scissors') {
-        let scissorsPic = document.createElement('img');
-        scissorsPic.setAttribute('src', '/images/skel-scissors.png');
-        scissorsPic.className = 'choicePic';
-        computerArea.appendChild(scissorsPic);
-    }
+    setTimeout(() => {
+        if (computerPick == 'rock') {
+            let rockPic = document.createElement('img');
+            rockPic.setAttribute('src', '/images/skel-rock.png');
+            rockPic.className = 'choicePic';
+            computerArea.appendChild(rockPic);
+        }
+    
+        if (computerPick == 'paper') {
+            let paperPic = document.createElement('img');
+            paperPic.setAttribute('src', '/images/skel-paper.png');
+            paperPic.className = 'choicePic';
+            computerArea.appendChild(paperPic);
+        }
+    
+        if (computerPick == 'scissors') {
+            let scissorsPic = document.createElement('img');
+            scissorsPic.setAttribute('src', '/images/skel-scissors.png');
+            scissorsPic.className = 'choicePic';
+            computerArea.appendChild(scissorsPic);
+        }
+    }, 1000);
+    
 }
 
 // results for if player wins, if computer wins, or draw
@@ -160,8 +194,18 @@ function gameRound(){
     if (playerPick === computerPick) {
         result = "It's a draw!";
     }
-
-    resultArea.innerHTML = result;
+    setTimeout(() => {
+        roundResult.innerHTML = result;
+    }, 2000);
+        
 }
+// reset button
+resetBtn.addEventListener('click', (e) => {
+    location.reload();
+})
+
+
+
+
 
 
